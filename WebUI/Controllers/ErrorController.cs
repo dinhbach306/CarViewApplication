d64b0352a3f8883c9a3db477dbed0283efabb1e4
magnus <dinhbach3062001@gmail.com>
2023-06-29 16:28:57 +0700

    add(webUI): adding a web ui for project

---
+++ WebUI/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers;
+
+public class ErrorController : Controller
+{
+    private readonly ILogger<ErrorController> _logger;
+
+    public ErrorController(ILogger<ErrorController> logger)
+    {
+        _logger = logger;
+    }
+    
+    public IActionResult Page404Error()
+    {
+        return View();
+    }
+    
+    public IActionResult Page500Error()
+    {
+        return View();
+    }
+}
